fda40c67139f284fb9dd5553b10936a36513cfaa
uncognic <92607100+uncognic@users.noreply.github.com>
2026-05-15 12:19:47 -0400

    update bootstrap script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cc -std=c23 -Isrc -Iextern -D_POSIX_C_SOURCE=200809L -o cast \
+cc -std=c23 -Iinclude -Iextern -D_POSIX_C_SOURCE=200809L -o cast \
     src/strbuf.c src/fs.c src/config.c src/build.c src/cli.c src/init.c src/main.c \
     extern/tomlc17.c
