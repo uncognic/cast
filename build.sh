a111ae31779a6e1c29799011254c857a98a62a5c
uncognic <uncognic@proton.me>
2026-05-16 18:09:23 -0400

    inject target info into build as CAST_NAME and CAST_VERSION

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cc -std=c23 -Iinclude -Iextern -D_POSIX_C_SOURCE=200809L -o cast \
+cc -std=c23 -Iinclude -Iextern -D_POSIX_C_SOURCE=200809L -DCAST_VERSION=\"dev\" -DCAST_NAME=\"cast\" -o cast \
     src/*.c \
     extern/tomlc17.c
 
